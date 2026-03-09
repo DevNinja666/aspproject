3d71bf1ef060e35604d1b9b88334dcddbb9a4b72
Elnur <elnurgasanovv07@gmail.com>
2026-03-09 23:57:34 +0400

    Add LoginUserDto for user login functionality

---
+++ DTOs/User/LoginUserDto.cs
@@ -0,0 +1,8 @@
+namespace Invoicer.DTOs.User
+{
+    public class LoginUserDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
