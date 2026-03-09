9aa4d5a5ce71023b87082714a77a36174dd45814
Elnur <elnurgasanovv07@gmail.com>
2026-03-10 00:00:19 +0400

    Create InvoiceRowDto.cs

---
+++ DTOs/InvoiceRowDto.cs
@@ -0,0 +1,9 @@
+namespace Invoicer.DTOs
+{
+    public class InvoiceRowDto
+    {
+        public string Service { get; set; } = null!;
+        public decimal Quantity { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
